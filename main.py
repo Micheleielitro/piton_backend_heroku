cc5072bcfd3978f6dcb89bdde8e0a30556019764
Michele I <mielitro@deloitte.it>
2023-09-07 14:13:06 +0100

    changed middlware logic

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import auth, admin, users, houses, squares, cities, house_features
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 app = FastAPI()
 
+origins = ["*"]
+
 models.Base.metadata.create_all(bind=engine)
 
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(admin.router)
 app.include_router(users.router)
 app.include_router(auth.router)
